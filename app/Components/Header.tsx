781fd6c03e93f295d9682bc2a88c874affdcafc2
Torres-EXT Luis (uie74314) <luis.3.torres-EXT@functionalaccount.com>
2025-03-28 22:49:26 -0600

    Changes styles

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,13 +1,13 @@
 export default function Header() {
     return (
-        <header className="fixed top-0 w-full mx-auto bg-sky-500/50">
+        <header className="fixed top-0 w-full mx-auto bg-sky-500/50 text-center">
             <nav className="flex items-center px-6 py-4 text-sm justify-between">
                 <div className="hidden md:flex flex-row justify-start">
                     <a href="/" className="inline-block">Ghp web app</a>
                 </div>
                 <div className="hidden md:flex flex-row justify-end">
-                    <a href="/examples" className="inline-block px-6">Examples</a>
-                    <a href="/Help" className="inline-block">Help</a>
+                    <a href="/examples" className="inline-block px-6 hover:text-blue-400">Examples</a>
+                    <a href="/help" className="inline-block px-6 hover:text-blue-400">Help</a>
                 </div>
             </nav>
         </header>
